46c7a6df69ad137f30a2fe0dd2f8476c35ea8cea
Ashmit Yadav <beingashmit@gmail.com>
2023-01-18 23:17:36 +0530

    day-1

---
+++ purplle/src/Components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+        <img src="https://fatafatsewa.com/website/images/emptycart.png" alt="" />
+         <h1>Add from Your wishlist</h1>
+    </div>
+  )
+}
+
+export default Cart
